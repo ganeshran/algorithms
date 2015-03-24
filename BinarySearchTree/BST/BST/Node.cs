e94e044d53668859346cb4107148588dfa758df2
Ganesh Ranganathan <ganesh.ranganathan.85@gmail.com>
2015-03-24 11:49:22 +0530

    adding the BST .NET project

---
+++ BinarySearchTree/BST/BST/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BST
+{
+    class Node
+    {
+        internal Node Left;
+        internal Node Right;
+        internal int Data;
+
+        internal Node(int d)
+        {
+            Data = d;
+
+        }
+    }
+}
