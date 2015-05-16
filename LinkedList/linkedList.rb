713ca1cfa3916bf3c99dae35f55855093e4ff294
= <ganesh.ranganathan.85@gmail.com>
2015-05-16 15:09:21 +0530

    Making node and linkedlist implementaiton in same file

--- LinkedList/linkedList.rb
+++ LinkedList/linkedList.rb
@@ -1,4 +1,11 @@
-require './node.rb'
+class Node
+	attr_accessor :value, :next_node
+
+	def initialize val, next_in_line
+		@value = val
+		@next_node = next_in_line
+	end
+end
 
 class LinkedList
 	attr_accessor :head
--- LinkedList/node.rb
+++
@@ -1,8 +0,0 @@
-class Node
-	attr_accessor :value, :next_node
-
-	def initialize val, next_in_line
-		@value = val
-		@next_node = next_in_line
-	end
-end
