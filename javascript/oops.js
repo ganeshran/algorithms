6d742d3f429b3b328541c6e2ee99773a056abbeb
= <ganesh.ranganathan.85@gmail.com>
2015-05-20 16:06:43 +0530

    prototypal inheritance

--- javascript/oops.js
+++ javascript/oops.js
@@ -3,7 +3,22 @@ function baseClass() {
 	this.property2 = "Hello world";
 
 	this.method1 = function fn0 (){
-		return this.property1
+		console.log("this is a method in the base class class");
 	};
 }
 
+function derivedClass() {
+	this.childProperty = 10;
+	};
+
+derivedClass.prototype = new baseClass();
+derivedClass.prototype.method1 = function fn1() {
+		console.log("this is a method in the derived class");
+	};
+ 
+
+
+var instance = new derivedClass();
+console.log(instance.property1);
+console.log(instance.childProperty);
+instance.method1();
