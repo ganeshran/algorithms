b37892028bb4a4bd06f5718a9d5b9a91a83a3991
= <ganesh.ranganathan.85@gmail.com>
2015-07-20 15:29:34 +0530

    adding some adhoc code

---
+++ adhoc/documentDistance.rb
@@ -0,0 +1,13 @@
+#computing document distance - analogous 
+
+#1 Split document into words
+#2 Compute word frequency
+#3 Compute dot product
+
+def documentDistance doc1,doc2
+	puts "Implmementing doc"
+end
+
+d1 = "the cat"
+d2 = "the dog"
+documentDistance(d1,d2)
