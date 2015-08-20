0d333fb651bd4a9a6cab7609ad0b66cbf4cbd964
Ganesh Ranganathan <ganesh.ranganathan.85@gmail.com>
2015-08-20 17:13:51 +0530

    Renaming solution and adding funny string

---
+++ Strings/StringDotNet/EliminateAdjacentDuplicates/FunnyString/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution {
+    static void Main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        int numberOfCases = int.Parse(Console.ReadLine());
+        for(int i = 0;i<numberOfCases;i++)
+            Console.WriteLine(CheckIfFunny(Console.ReadLine()) ? "Funny" : "Not Funny");
+    }
+    
+     static bool CheckIfFunny(string input)
+        {
+            for (int i = 0; i < input.Length -1 ; i++)
+            {
+                int j = input.Length - i - 1;
+                if (Math.Abs(((int) input[i]) - ((int) input[i + 1])) != Math.Abs(((int) input[j]) - (int) input[j - 1]))
+                    return false;
+            }
+            return true;
+        }
+    
+    
+}
+
+    
