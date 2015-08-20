af9f14aa3bd70c1585ddd4335216803d4dfedb8a
Ganesh Ranganathan <ganesh.ranganathan.85@gmail.com>
2015-08-21 04:00:39 +0530

    adding two strings

---
+++ Strings/StringDotNet/StringProblems/TwoStrings/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TwoStrings("hello","world"));
+        }
+
+        static bool TwoStrings(string str1, string str2)
+        {
+            int[] arr1 = new int[26];
+
+            for (int i = 0; i < str1.Length; i++)
+            {
+                arr1[(int) str1[i] - 97] = 1;
+            }
+
+            for (int i = 0; i < str2.Length; i++)
+            {
+                if (arr1[(int) str2[i] - 97] == 1)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
