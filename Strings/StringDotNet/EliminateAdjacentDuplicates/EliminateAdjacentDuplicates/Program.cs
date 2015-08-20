25bbb06d31c5c063e459e8652efda312a3791243
Ganesh Ranganathan <ganesh.ranganathan.85@gmail.com>
2015-08-20 16:00:44 +0530

    adding iterative string function

--- Strings/StringDotNet/EliminateAdjacentDuplicates/EliminateAdjacentDuplicates/Program.cs
+++ Strings/StringDotNet/EliminateAdjacentDuplicates/EliminateAdjacentDuplicates/Program.cs
@@ -10,6 +10,56 @@ namespace EliminateAdjacentDuplicates
     {
         static void Main(string[] args)
         {
+            TestInput(new string[]{"ABBC","ACBBC","ABBBCDDC","A","ABC"}); 
+
+        }
+
+        public static void TestInput(IEnumerable<string> inputArray)
+        {
+            var rem = new RemoveDuplicates();
+            foreach (var input in inputArray)
+            {
+                Console.WriteLine("Input: {0}, Output: {1}",input,rem.RemoveUsingStack(input));
+            }
+        }
+
+      
+    }
+
+
+    internal class RemoveDuplicates
+    {
+        public string RemoveUsingStack(string input)
+        {
+            // Stack is useless for this solution because too many string manipulation
+            var stack = new Stack<char>();
+            var builder = new StringBuilder();
+            for (int i = 0; i < input.Length; i++)
+            {
+
+                if (stack.Count == 0 || stack.Peek() != input[i])
+                    stack.Push(input[i]);
+                else
+                {
+                    stack.Pop();
+                }
+            }
+
+            while (stack.Count > 0)
+            {
+                builder.Append(stack.Pop());
+            }
+            //VERY POOR CODE.
+            var charArray = builder.ToString().ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        public string RemoveIteratively(string input)
+        {
+            int i = 0,j = 0;
+
+            return input;
         }
     }
 }
