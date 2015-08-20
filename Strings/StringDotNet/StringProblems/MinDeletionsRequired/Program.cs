62fb6949103d5fb6a8be235b653c9bca064b601d
Ganesh Ranganathan <ganesh.ranganathan.85@gmail.com>
2015-08-20 18:09:21 +0530

    solving min deletions required

---
+++ Strings/StringDotNet/StringProblems/MinDeletionsRequired/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinDeletionsRequired
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MinDeletionsRequired("AAABBB"));
+        }
+
+
+        static int MinDeletionsRequired(string input)
+        {
+            int deletions = 0;
+            var stack = new Stack<char>();
+            stack.Push(input[0]);
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (stack.Count > 0 && stack.Peek() == input[i])
+                    deletions++;
+                else
+                    stack.Push(input[i]);
+            }
+
+            return deletions;
+        }
+    }
+}
