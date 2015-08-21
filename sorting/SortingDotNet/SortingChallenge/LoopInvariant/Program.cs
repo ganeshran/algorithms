acabc5d55173c0b5a8c73d64ff7a5a001edf528c
Ganesh Ranganathan <ganesh.ranganathan.85@gmail.com>
2015-08-22 00:48:24 +0530

    adding the loop invariant challenge solution

---
+++ sorting/SortingDotNet/SortingChallenge/LoopInvariant/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopInvariant
+{
+    class Program
+    {
+        public static void insertionSort(int[] A)
+        {
+            var j = 0;
+            for (var i = 1; i < A.Length; i++)
+            {
+                var value = A[i];
+                j = i - 1;
+                while (j >= 0 && value < A[j])
+                {
+                    A[j + 1] = A[j];
+                    j = j - 1;
+                }
+                A[j + 1] = value;
+            }
+            Console.WriteLine(string.Join(" ", A));
+        }
+
+        static void Main(string[] args)
+        {
+//            int[] newAr = new int[] {1, 4, 3, 5, 6, 2};
+            Console.ReadLine();
+            int[] _ar = (from s in Console.ReadLine().Split() select Convert.ToInt32(s)).ToArray();
+            insertionSort(_ar); 
+        }
+    }
+}
